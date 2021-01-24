bfb14bb9ba994e3214c3740a81be878ff5db55bd
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-01-24 13:03:04 +0000

    Number Convertors

---
+++ index.js
@@ -0,0 +1,43 @@
+const readLineSync = require('readline-sync');
+
+const typeOfConversion = readLineSync.question(`Hi, Welcome to Unit Convertor\n\nWe have following convertors:\n1. Binary To Decimal\n2. Binary To Hex\n3. Binary To Octal\n4. Decimal To Binary\n5. Decimal To Hex\n6. Decimal To Octal\n7. Hex To Binary\n8. Hex To Decimal\n9. Hex To Octal\n10. Octal To Binary\n11. Octal To Decimal\n12. Octal To Hex\nEnter your option\n`);
+
+const inputValue = readLineSync.question(`Enter the value to convert\n`);
+
+switch(typeOfConversion){
+  case '1':
+   break;
+  
+  case '2':
+   break;
+
+  case '3':
+   break;
+
+  case '4':
+   break;
+
+  case '5':
+   break;
+
+  case '6':
+   break;
+
+  case '7':
+   break;
+
+  case '8':
+   break;
+
+  case '9':
+   break;
+
+  case '10':
+   break;
+
+  case '11':
+   break;
+
+  case '12':
+   break;
+}
